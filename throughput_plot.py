99615b75fffd86016b240258f733db04f5754da7
Shravan Narayan <shravanrn@gmail.com>
2024-06-24 00:25:13 -0500

    Update FaaS benchmark with real data

--- throughput_plot.py
+++ throughput_plot.py
@@ -23,21 +23,42 @@ def generate_graph(test_x, testtimingsarray_map, outputFile):
         plt.plot(test_x, measurement, label=attribute, color=colors[idx], marker='o')
 
     plt.xticks(np.arange(min(test_x), max(test_x)+1, 1))
+    plt.yticks(np.arange(0, 25, 5))
     ax.legend(loc='upper left', ncols=1)
     ax.grid(axis="y", linestyle="dotted")
     ax.yaxis.set_major_formatter(mtick.FormatStrFormatter('%.0f%%'))
     ax.yaxis.set_label_coords(-0.09,0.45)
     plt.subplots_adjust(left=0.11, right=1, top=0.99, bottom=0.23)
 
-
-
     plt.savefig(outputFile, format="pdf")
 
 def main():
     test_x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+
+    multiproc_time_hash = [864, 1786, 2684, 3630, 4685, 5708, 6679, 7942, 8845, 10147, 11296, 12541, 13611, 14944, 16200]
+    colorguard_time_hash = [848, 1708, 2549, 3360, 4227, 5058, 5869, 6710, 7546, 8404, 9227, 10103, 10968, 11741, 12562]
+    assert(len(multiproc_time_hash) == len(colorguard_time_hash))
+
+    jobs_per_process = 14582
+    multiproc_throughput_hash = []
+    colorguard_throughput_hash = []
+    throughputdrop_hash = []
+
+    for i in range(len(multiproc_time_hash)):
+        jobs = jobs_per_process * (i + 1)
+        multiproc_throughput_hash.append(jobs / multiproc_time_hash[i])
+        colorguard_throughput_hash.append(jobs / colorguard_time_hash[i])
+        throughputdrop_hash.append(100 * (colorguard_throughput_hash[i] - multiproc_throughput_hash[i]) / colorguard_throughput_hash[i])
+
+    print("Hash-based load-balance")
+    print("------------------------")
+    print("multiproc_throughput_hash: " + str(multiproc_throughput_hash))
+    print("colorguard_throughput_hash: " + str(colorguard_throughput_hash))
+    print("throughputdrop_hash: " + str(throughputdrop_hash))
+
     testtimingsarray_map = {
-        "Hash-based load-balance" : [2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30],
-        "HTML templating"         : [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16],
+        "Hash-based load-balance" : throughputdrop_hash,
+        # "HTML templating"         : [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16],
     }
     outputdir = os.path.dirname(os.path.realpath(__file__))
     benchfile = os.path.join(outputdir, "benchmarks", "faas-throughput.pdf")
