7763d22067ee2585f1f29159a076ffa4440f499f
Shravan Narayan <shravanrn@gmail.com>
2024-06-24 19:02:11 -0500

    Add regex filtering bench

--- throughput_plot.py
+++ throughput_plot.py
@@ -32,32 +32,45 @@ def generate_graph(test_x, testtimingsarray_map, outputFile):
 
     plt.savefig(outputFile, format="pdf")
 
+def compute(multiproc_time, colorguard_time):
+    assert(len(multiproc_time) == len(colorguard_time))
+    jobs_per_process = 14582
+    multiproc_throughput = []
+    colorguard_throughput = []
+    throughputinc = []
+
+    for i in range(len(multiproc_time)):
+        jobs = jobs_per_process * (i + 1)
+        multiproc_throughput.append(jobs / multiproc_time[i])
+        colorguard_throughput.append(jobs / colorguard_time[i])
+        throughputinc.append(100 * (colorguard_throughput[i] - multiproc_throughput[i]) / multiproc_throughput[i])
+
+    print("multiproc_throughput: " + str(multiproc_throughput))
+    print("colorguard_throughput: " + str(colorguard_throughput))
+    print("throughputdrop: " + str(throughputinc))
+
+    return throughputinc
+
 def main():
     test_x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 
-    multiproc_time_hash = [864, 1786, 2684, 3630, 4685, 5708, 6679, 7942, 8845, 10147, 11296, 12541, 13611, 14944, 16200]
-    colorguard_time_hash = [848, 1708, 2549, 3360, 4227, 5058, 5869, 6710, 7546, 8404, 9227, 10103, 10968, 11741, 12562]
-    assert(len(multiproc_time_hash) == len(colorguard_time_hash))
-
-    jobs_per_process = 14582
-    multiproc_throughput_hash = []
-    colorguard_throughput_hash = []
-    throughputinc_hash = []
-
-    for i in range(len(multiproc_time_hash)):
-        jobs = jobs_per_process * (i + 1)
-        multiproc_throughput_hash.append(jobs / multiproc_time_hash[i])
-        colorguard_throughput_hash.append(jobs / colorguard_time_hash[i])
-        throughputinc_hash.append(100 * (colorguard_throughput_hash[i] - multiproc_throughput_hash[i]) / multiproc_throughput_hash[i])
+    multiproc_time_hash = [864, 1786, 2684, 3630, 4685, 5708, 6679, 7942, 8845, 10147, 11296, 12541, 13611, 14944, 16200,]
+    colorguard_time_hash = [848, 1708, 2549, 3360, 4227, 5058, 5869, 6710, 7546, 8404, 9227, 10103, 10968, 11741, 12562,]
 
     print("Hash-based load-balance")
     print("------------------------")
-    print("multiproc_throughput_hash: " + str(multiproc_throughput_hash))
-    print("colorguard_throughput_hash: " + str(colorguard_throughput_hash))
-    print("throughputdrop_hash: " + str(throughputinc_hash))
+    throughputinc_hash = compute(multiproc_time_hash, colorguard_time_hash)
+
+    multiproc_time_regex = [1926, 3902, 5921, 8107, 10236, 12561, 14834, 17314, 19849, 22356, 24744, 27544, 30064, 32552, 35302,]
+    colorguard_time_regex = [1900, 3738, 5609, 7468, 9309, 11174, 13011, 14845, 16683, 18570, 20380, 22277, 24124, 25905, 27738,]
+
+    print("Regex filtering")
+    print("------------------------")
+    throughputinc_regex = compute(multiproc_time_regex, colorguard_time_regex)
 
     testtimingsarray_map = {
         "Hash-based load-balance" : throughputinc_hash,
+        "Regex filtering" : throughputinc_regex,
         # "HTML templating"         : [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16],
     }
     outputdir = os.path.dirname(os.path.realpath(__file__))
